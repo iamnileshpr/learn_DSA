1d90f0708af4fdee44e5f9272e9278bfa91a8b00
iamnileshpr <nilesh.rajak123456@gmail.com>
2026-04-08 23:52:14 +0530

    today

--- .vscode/RemoveDupII.cpp
+++ .vscode/RemoveDupII.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
-    int findShortestSubArray(vector<int>& nums) {
-
-        unordered_map<int,int> freq;
-        unordered_map<int,int> first;
-        unordered_map<int,int> last;
-
-        int n = nums.size();
-
-        // Step 1: Fill maps
-        for(int i = 0; i < n; i++){
-            int x = nums[i];
-
-            freq[x]++;
-
-            if(first.find(x) == first.end()){
-                first[x] = i;
-            }
-
-            last[x] = i;
-        }
-
-        // Step 2: Find degree
-        int degree = 0;
-        for(auto it : freq){
-            degree = max(degree, it.second);
-        }
-
-        // Step 3: Find minimum length
-        int ans = INT_MAX;
-
-        for(auto it : freq){
-            if(it.second == degree){
-                int x = it.first;
-                int len = last[x] - first[x] + 1;
-                ans = min(ans, len);
+    int removeDuplicates(vector<int>& nums) {
+        if(nums.size()<=2) return nums.size();
+        int k=2;
+        for(int i=2;i<nums.size();i++){
+            if(nums[i]!=nums[k-2]){
+                nums[k]=nums[i];
+                k++;
             }
         }
-
-        return ans;
+        return k;
+        
     }
 };
