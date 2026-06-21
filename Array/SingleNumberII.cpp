43f969789112c0b6da49cea5fd5111aff2a6044c
iamnileshpr <nilesh.rajak123456@gmail.com>
2026-06-21 23:59:48 +0530

    new1

---
+++ Array/SingleNumberII.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+
+        int ans = 0;
+
+        for(int bit = 0; bit < 32; bit++) {
+
+            int count = 0;
+
+            for(int num : nums) {
+
+                if((num >> bit) & 1)
+                    count++;
+            }
+
+            if(count % 3 != 0) {
+                ans = ans | (1 << bit);
+            }
+        }
+
+        return ans;
+    }
+};
